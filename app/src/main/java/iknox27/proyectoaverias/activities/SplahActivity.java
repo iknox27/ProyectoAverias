9d0a09d2e04f1f68d7ae3700388d9cf25a5567b8
iknox27 <acam27@hotmail.com>
2018-07-07 13:52:31 -0600

    cambios iniciales

--- app/src/main/java/iknox27/proyectoaverias/activities/SplahActivity.java
+++ app/src/main/java/iknox27/proyectoaverias/activities/SplahActivity.java
@@ -1,15 +1,25 @@
 package iknox27.proyectoaverias.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import iknox27.proyectoaverias.R;
 
 public class SplahActivity extends AppCompatActivity {
-
+  private final int SPLASH_DISPLAY_LENGTH = 2000;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splah);
+    new Handler().postDelayed(new Runnable(){
+      @Override
+      public void run() {
+        Intent mainIntent = new Intent(SplahActivity.this,UserActivity.class);
+        startActivity(mainIntent);
+        finish();
+      }
+    }, SPLASH_DISPLAY_LENGTH);
   }
 }
--- app/src/main/java/iknox27/proyectoaverias/activities/UserActivity.java
+++ app/src/main/java/iknox27/proyectoaverias/activities/UserActivity.java
@@ -1,9 +1,13 @@
 package iknox27.proyectoaverias.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import iknox27.proyectoaverias.R;
+import iknox27.proyectoaverias.fragments.LoginFragment;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -11,5 +15,16 @@ public class UserActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_user);
+    setFragment(new LoginFragment());
+  }
+
+  
+
+  protected void setFragment(Fragment fragment) {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction =
+            fragmentManager.beginTransaction();
+    fragmentTransaction.replace(android.R.id.content, fragment);
+    fragmentTransaction.commit();
   }
 }
